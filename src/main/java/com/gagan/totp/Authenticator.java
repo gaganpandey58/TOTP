2a0ac37fe34430bfe9d3a83f8a4e8b53bd4e6469
Gagan Pandey <gpandey@deerhold.com>
2023-11-27 20:19:07 +0545

    refactor the chunky codes

--- src/main/java/com/gagan/totp/Authenticator.java
+++ src/main/java/com/gagan/totp/Authenticator.java
@@ -6,6 +6,8 @@ import org.apache.commons.codec.binary.Base32;
 import org.apache.commons.codec.binary.Base64;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public final class Authenticator {
@@ -16,38 +18,57 @@ public final class Authenticator {
     }
 
     String calculateCode(byte[] key, long tm) {
-        byte[] data = new byte[8];
-        long value = tm;
+        byte[] data = prepareData(tm);
+        SecretKeySpec signKey = getSignKey(key);
+        byte[] hash = computeHash(data, signKey);
 
-        for (int i = 8; i-- > 0; value >>>= 8) {
-            data[i] = (byte) value;
-        }
+        long truncatedHash = calculateTruncatedHash(hash);
+        truncatedHash %= config.getKeyModulus();
 
-        SecretKeySpec signKey = new SecretKeySpec(key, config.getHashFunction().toString());
+        return formatTruncatedHash(truncatedHash);
+    }
 
+    byte[] computeHash(byte[] data, SecretKeySpec signKey) {
         try {
-            Mac mac = Mac.getInstance(config.getHashFunction().toString());
-
-            mac.init(signKey);
-
-            byte[] hash = mac.doFinal(data);
-
-            int offset = hash[hash.length - 1] & 0xF;
-            long truncatedHash = 0;
-
-            for (int i = 0; i < 4; ++i) {
-                truncatedHash <<= 8;
-                truncatedHash |= (hash[offset + i] & 0xFF);
-            }
-
-            truncatedHash &= 0x7FFFFFFF;
-            truncatedHash %= config.getKeyModulus();
-
-            return String.format("%06d", (int) truncatedHash);
+            return calculateHash(data, signKey);
+        } catch (Exception exception) {
+            throw new AuthenticatorException("An error occurred while computing hash", exception.getCause());
         }
-        catch (Exception exception) {
-            throw new AuthenticatorException("An error occurred while generating OTP code", exception.getCause());
+    }
+
+    String formatTruncatedHash(long truncatedHash) {
+        return String.format("%06d", (int) truncatedHash);
+    }
+
+    byte[] prepareData(long tm) {
+        byte[] data = new byte[8];
+        for (int i = 8; i-- > 0; tm >>>= 8) {
+            data[i] = (byte) tm;
         }
+        return data;
+    }
+
+    SecretKeySpec getSignKey(byte[] key) {
+        String hashFunction = config.getHashFunction().toString();
+        return new SecretKeySpec(key, hashFunction);
+    }
+
+    byte[] calculateHash(byte[] data, SecretKeySpec signKey) throws NoSuchAlgorithmException, InvalidKeyException {
+        String hashFunction = config.getHashFunction().toString();
+        Mac mac = Mac.getInstance(hashFunction);
+        mac.init(signKey);
+        return mac.doFinal(data);
+    }
+
+    long calculateTruncatedHash(byte[] hash) {
+        int offset = hash[hash.length - 1] & 0xF;
+        long truncatedHash = 0;
+        for (int i = 0; i < 4; ++i) {
+            truncatedHash <<= 8;
+            truncatedHash |= (hash[offset + i] & 0xFF);
+        }
+        truncatedHash &= 0x7FFFFFFF;
+        return truncatedHash;
     }
 
     private long getTimeWindowFromTime(long time) {
@@ -57,14 +78,22 @@ public final class Authenticator {
     private byte[] decodeSecret(String secret) {
         switch (config.getEncoder()) {
             case BASE32:
-                Base32 codec32 = new Base32();
-                return codec32.decode(secret.toUpperCase());
+                return decodeBase32Secret(secret);
             case BASE64:
-                Base64 codec64 = new Base64();
-                return codec64.decode(secret);
+                return decodeBase64Secret(secret);
             default:
                 throw new AuthenticatorException("Unknown Encode type.");
         }
+    }
+
+    private byte[] decodeBase32Secret(String secret) {
+        Base32 base32Codec = new Base32();
+        return base32Codec.decode(secret.toUpperCase());
+    }
+
+    private byte[] decodeBase64Secret(String secret) {
+        Base64 base64Codec = new Base64();
+        return base64Codec.decode(secret);
     }
 
     public String getTotpPassword(String secret) {
